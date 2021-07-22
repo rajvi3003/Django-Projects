8fdd68a9633bfb027eaba2a0e7a15f1c83fdde17
paras chhugani <peoband@gmail.com>
2021-07-23 01:44:56 +0530

    upload data with redirector

--- SonicRes/views.py
+++ SonicRes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.http import HttpResponse
 from .models import testupload
 
@@ -18,16 +18,22 @@ def upload(request):
         test = testupload(filex = request.FILES['myFile'] ,title = str(request.FILES['myFile']) , shortUrl = '')
         test.save()
         print(test.filex)
-        redirectLink = 'http://127.0.0.1:8000/media/'+str(test.filex)
-        test.shortUrl =  'http://127.0.0.1:8000/go/'+shortner(redirectLink)
+        redirectLink = 'https://parasdjango.pythonanywhere.com/media/'+str(test.filex)
+        # redirectLink = 'http://127.0.0.1:8000/media/'+str(test.filex)
+
+        test.shortUrl =  'https://parasdjango.pythonanywhere.com/go/'+shortner(redirectLink)
+        # test.shortUrl =  'http://127.0.0.1:8000/go/'+shortner(redirectLink)
         test.save()
         print("Uploaded the file" , request.FILES['myFile'])
-        return HttpResponse(str(test.shortUrl))
+        # return HttpResponse(str(test.shortUrl))
+        return redirect('/SonicRes/dashboard')
+
 
 #only used to shortn the file url
 def shortner(redirectLink):
     Redirectdata = {'link': redirectLink}
-    shortUrl = requests.post(url = 'http://127.0.0.1:8000/Urlshortner/create', data = Redirectdata)
+    shortUrl = requests.post(url = 'https://parasdjango.pythonanywhere.com/Urlshortner/create', data = Redirectdata)
+    # shortUrl = requests.post(url = 'http://127.0.0.1:8000/Urlshortner/create', data = Redirectdata)
     shortUrl = shortUrl.text
     return shortUrl
 
